e9e74ca020d2ff06840245b63d8c8c66d4b6dd49
yannismrad <yannismrad@free.fr>
2018-03-25 19:00:08 -0400

    Wandering AIs WIP

---
+++ Assets/Scripts/Gameplay/WaypointsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointsManager : MonoBehaviour {
+
+    public static WaypointsManager _instance = null;
+    public List<GameObject> waypoints;
+
+    public void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+        }
+        else if(_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static WaypointsManager GetWaypointsManager()
+    {
+        return _instance;
+    }
+}
