90baa825f28f1ab9de94ddb3ba6062c4c304c1d4
Samuel <carriere911@gmail.com>
2018-03-28 21:39:38 -0400

    base zombie ai

---
+++ Assets/Scripts/Controllers/ZombieController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    public GameObject target;
+    public float radius = 10.0f;
+    private const string PLAYER_TAG = "player";
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    private void FindTarget()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        Collider nearestCollider = null;
+        float minSqrDistance = Mathf.Infinity;
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].tag == PLAYER_TAG)
+            {
+                float sqrDistanceToCenter = (transform.position - colliders[i].transform.position).sqrMagnitude;
+
+                if (sqrDistanceToCenter < minSqrDistance)
+                {
+                    minSqrDistance = sqrDistanceToCenter;
+                    nearestCollider = colliders[i];
+                }
+            }
+        }
+        if (nearestCollider != null)
+        {
+            target = nearestCollider.gameObject;
+        }
+    }
+}
