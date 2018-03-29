1dc9f61d53af53bbda1bdaaf43fa536fdb148968
Samuel <carriere911@gmail.com>
2018-03-28 22:15:10 -0400

    add the follow player once a target is choosen

--- Assets/Scripts/Controllers/ZombieController.cs
+++ Assets/Scripts/Controllers/ZombieController.cs
@@ -1,23 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ZombieController : MonoBehaviour
 {
     public GameObject target;
     public float radius = 10.0f;
+    public float speed = 3.0F;
+
     private const string PLAYER_TAG = "player";
+    private Vector3 _moveInput;
+    private Vector3 _moveVelocity;
+    private Rigidbody _rigidBody;
 
     void Start()
     {
+        _rigidBody = GetComponent<Rigidbody>();
+        FindTarget();
     }
 
     void Update()
     {
         if (target == null)
         {
+            //Roaming
             FindTarget();
         }
+        else
+        {
+            _moveInput = (target.transform.position - transform.position).normalized;
+            _moveVelocity = _moveInput * speed;
+            Debug.DrawLine(transform.position, _moveVelocity + Vector3.up, Color.red);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //TODO: can remove the gravity by removing the up vector
+        _rigidBody.velocity = _moveVelocity + _rigidBody.velocity.y * Vector3.up;
     }
 
     private void OnDrawGizmos()
