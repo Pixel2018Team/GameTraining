dfce8fe79d35a99900baa89d6ecf99a6a82ae70d
Leandre <leandremonette@gmail.com>
2018-04-03 18:44:29 -0700

    wwise integration

---
+++ Assets/Scripts/Audio/AudioTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour {
+	void Start(){
+		Debug.Log ("Test sound is playing");
+		AkSoundEngine.PostEvent ("test", gameObject);
+	}
+}
